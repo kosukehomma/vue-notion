73f6dceaf5a32883d616ea2a4d08a3baa44afb26
honmakousuke <kou4321ske@gmail.com>
2022-02-26 20:59:28 +0900

    chapter7-2 localStorage add

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="main-page">
     <div class="left-menu" @click.self="onEditNoteEnd()">
+      <!-- 保存ボタン -->
+      <button class="transparent" @click.self="onClickButtonSave">
+        <i class="fas fa-save"></i> 内容を保存
+      </button>
+
       <!-- ノートリスト -->
       <draggable v-bind:list="noteList" group="notes">
         <NoteItem
@@ -63,6 +68,12 @@ export default {
     return {
       noteList: [],
       selectedNote: null,
+    }
+  },
+  created: function() {
+    const localData = localStorage.getItem('noteItem');
+    if (localData != null) {
+      this.noteList = JSON.parse(localData);
     }
   },
   methods: {
@@ -160,10 +171,19 @@ export default {
       const index = targetList.indexOf(widget);
       targetList.splice(index, 1);
 
+      // 削除した1つ前のウィジェットを選択状態にする
       const focusWidget = (index === 0) ? parentWidget : targetList[index - 1];
       if (focusWidget != null) {
         focusWidget.id = (parent(focusWidget.id, 16) + 1).toString(16);
       }
+    },
+    onClickButtonSave : function() {
+      localStorage.setItem('noteItem', JSON.stringify(this.noteList));
+      this.$toasted.show('ノートを保存しました', {
+        position: 'top-left',
+        duration: 1000,
+        type: 'success'
+      });
     },
   },
   computed: {
