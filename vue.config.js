c590974eb385a7384ed622c2f3edc76170b9d6d1
honmakousuke <kou4321ske@gmail.com>
2022-02-26 21:31:57 +0900

    capter7-3 favicon modified

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "MyNotion like memo",
+    }
+  },
   devServer: {
     port: 8083,
     disableHostCheck: true,
